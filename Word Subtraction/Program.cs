581e731deb2b17e33ac5689440db9d20d87449ba
P Dring <pddring@googlemail.com>
2020-09-22 10:09:11 +0100

    Add project files.

---
+++ Word Subtraction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Word_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user to enter two words
+            Console.WriteLine("Please enter a word");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Please enter a second word");
+            string secondWord = Console.ReadLine();
+
+            // convert to ASCII
+            byte [] asciiFirstWord = Encoding.ASCII.GetBytes(firstWord);
+            byte[] asciiSecondWord = Encoding.ASCII.GetBytes(secondWord);
+            int[] asciiSubtracted = new int[firstWord.Length];
+
+            // Loop through and do the subtraction
+            for (int i = 0; i < asciiFirstWord.Length; i++)
+            {
+                asciiSubtracted[i] = asciiFirstWord[i] - asciiSecondWord[i];
+            }
+
+            for(int i = 0; i < asciiSubtracted.Length; i++)
+            {
+                Console.WriteLine(asciiSubtracted[i]);
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
